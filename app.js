b4b82ef9208022c58a15c8f1a11843fd7dac0713
rodionkuhty <rodionkuhty@gmail.com>
2018-04-13 20:55:03 +0300

    add more css

--- app.js
+++ app.js
@@ -16,5 +16,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get('/', function(req, res){
     res.render('index');
 });
+app.get('/about', function(req, res){
+   res.render('about');
+});
 app.listen(3000);
 console.log('Server is running on port 3000');
